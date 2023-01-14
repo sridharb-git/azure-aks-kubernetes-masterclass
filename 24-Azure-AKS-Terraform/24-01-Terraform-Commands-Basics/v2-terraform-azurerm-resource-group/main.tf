2ffc2cf48c52e48d9c95ca73c092e7f1084e2b61
sridharb-git <asu.babu09@gmail.com>
2023-01-14 09:43:08 +0530

    commit all

--- 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v2-terraform-azurerm-resource-group/main.tf
+++ 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v2-terraform-azurerm-resource-group/main.tf
@@ -10,14 +10,14 @@ provider "azurerm" {
  
 # Create a Azure Resource Group
 resource "azurerm_resource_group" "aks-rg2" {
-  name     = "aks-rg2-tf"
+  name     = "aks-rg2-tf2"
   location = "Central US"
 
 # Add Tags
-#  tags = {
-#    "environment" = "k8sdev"
-#    "demotag"     = "refreshtest"
-#  }
+tags = {
+  "environment" = "k8sdev"
+  "demotag"     = "refreshtest"
+}
 
 }
 
